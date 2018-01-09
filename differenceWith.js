c92523d777a3aea4ba11369f68b056fbc6a35d30
rickkim90 <rickkim90@gmail.com>
2018-01-09 21:01:35 +0900

    diffrenceWith

---
+++ differenceWith.js
@@ -0,0 +1,3 @@
+const differenceWith = (arr, val, comp) => arr.filter(a => val.findIndex(b => comp(a, b)) === -1);
+
+differenceWith([1, 1.2, 1.5, 3, 0], [1.9, 3, 0], (a, b) => Math.round(a) === Math.round(b)); // [1, 1.2]
